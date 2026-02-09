e12d970ac40b8f25cd4bb60c79ca9a19f7bfedd3
Avetis <avetis.berberyan.techx@gmail.com>
2026-02-09 14:17:25 +0400

    add the header block

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import logo from "../assets/stars.svg";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="header__left">
+        <div className="header__logo-wrapper">
+          <img src={logo} alt="Logo" className="header__logo" />
+        </div>
+        <h1 className="header__title">Personal Dashboard</h1>
+      </div>
+
+      <div className="header__right">
+        <p className="header__text">Please log in to access your dashboard</p>
+      </div>
+    </header>
+  );
+}
